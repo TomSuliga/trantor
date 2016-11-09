c977960bc0f3c24292e83fa9828241b54fc396ca
tom <tomsuliga@users.noreply.github.com>
2016-11-09 13:50:08 -0700

    Http property; Earthquake junit

--- src/test/java/org/suliga/TrantorApplicationTests.java
+++ src/test/java/org/suliga/TrantorApplicationTests.java
@@ -3,16 +3,25 @@ package org.suliga;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.suliga.trantor.service.earthquake.EarthquakeService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TrantorApplicationTests {
 
+	@Autowired
+	private EarthquakeService earthquakeService;
+	
 	@Test
 	public void contextLoads() {
 		Assert.assertTrue(true);
 	}
-
+	
+	@Test
+	public void earthquakeService() {
+		Assert.assertNotNull(earthquakeService);
+	}
 }
