762f15754b745c74b464f385dca9a613c33570cf
tom <tomsuliga@users.noreply.github.com>
2016-11-11 15:24:40 -0700

    Removed unused service file.  Started adding in Crossword changes

--- src/main/java/org/suliga/trantor/controller/TrantorMainController.java
+++ src/main/java/org/suliga/trantor/controller/TrantorMainController.java
@@ -1,16 +1,22 @@
 package org.suliga.trantor.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.suliga.trantor.service.crossword.CrosswordPuzzleService;
 import org.suliga.trantor.service.earthquake.EarthquakeService;
 
 @Controller
 public class TrantorMainController {
 	@Autowired
 	private EarthquakeService earthquakeService;
+	
+	@Autowired
+	private CrosswordPuzzleService crosswordPuzzleService;
 	
 	@RequestMapping("/")
 	public String home(Model model) { // Model = interface, ModelMap = class
@@ -22,6 +28,17 @@ public class TrantorMainController {
 	public String getEarthquake(Model model, String period) {
 		model.addAttribute("eqdata", earthquakeService.getTopSixEvents(period));
 		return "earthquake";
+	}
+	
+	@RequestMapping(value = "/crossword", method = RequestMethod.GET)
+	public String getMobyDickCrosswordPuzzle(Model model, HttpServletRequest request) {
+		String session = request.getSession().toString();
+		//System.out.println("Session = " + session);
+		model.addAttribute("title", "Moby-Dick Crossword Puzzle");
+		model.addAttribute("grid", crosswordPuzzleService.getGrid(session));
+		model.addAttribute("httpSession",session);
+		model.addAttribute("autoCheck", crosswordPuzzleService.getAutoCheck(session));
+		return "crossword";
 	}
 	
 	@RequestMapping("/admin")
--- src/main/java/org/suliga/trantor/service/CrosswordService.java
+++
@@ -1,5 +0,0 @@
-package org.suliga.trantor.service;
-
-public interface CrosswordService {
-
-}
