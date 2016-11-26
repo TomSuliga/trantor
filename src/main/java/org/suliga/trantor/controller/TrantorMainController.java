513d0764d6970bfa52e621442cabeb852e7be45d
tom <tom2wheeler@gmail.com>
2016-11-26 10:15:48 -0700

    Added file upload capability

--- src/main/java/org/suliga/trantor/controller/TrantorMainController.java
+++ src/main/java/org/suliga/trantor/controller/TrantorMainController.java
@@ -1,5 +1,7 @@
 package org.suliga.trantor.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,8 +10,13 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.suliga.trantor.model.Person;
 import org.suliga.trantor.service.MinesweeperService;
 import org.suliga.trantor.service.crossword.CrosswordPuzzleService;
@@ -25,6 +32,9 @@ public class TrantorMainController {
 	
 	@Autowired
 	private MinesweeperService minesweeperService;
+	
+	@Autowired
+    private HttpServletRequest request;
 	
 	@RequestMapping("/")
 	public String home(Model model) { // Model = interface, ModelMap = class
@@ -88,6 +98,41 @@ public class TrantorMainController {
 		model.addAttribute("id","123XYZ");
 		return "jsp/try1";
 	}
+	
+	@GetMapping("/myupload")
+    public String handleFileUploadGet(Model model) {
+        return "myupload";
+    }
+	
+	@PostMapping("/myupload")
+    public String handleFileUploadPost(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) {
+
+		File file2 = new File("temp/" + file.getOriginalFilename());
+		System.out.println("file2=" + file2.getAbsolutePath());
+		try {
+            String uploadsDir = "/uploads/";
+            String realPathtoUploads =  request.getServletContext().getRealPath(uploadsDir);
+            if(! new File(realPathtoUploads).exists())
+            {
+                new File(realPathtoUploads).mkdir();
+            }
+
+            System.out.println("realPathtoUploads = " + realPathtoUploads);
+
+            String orgName = file.getOriginalFilename();
+            String filePath = realPathtoUploads + orgName;
+            File dest = new File(filePath);
+            file.transferTo(dest);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        //storageService.store(file);
+        redirectAttributes.addFlashAttribute("message",
+                "You successfully uploaded " + file.getOriginalFilename() + "!");
+
+        return "redirect:/myupload";
+    }
 }
 
 
